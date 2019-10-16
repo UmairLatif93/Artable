6e0a576405a309c5e60d737f15f9235fea4be930
Umair Latif <m.umair.latif1@gmail.com>
2019-10-16 15:34:10 +0500

    Add second Target for Admin and Shared Folder

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Umair Latif on 16/10/2019.
+//  Copyright © 2019 Umair Latif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
